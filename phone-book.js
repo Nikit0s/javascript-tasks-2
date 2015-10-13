fcb7020556b63d20e74a5c2b636f2074ede5ff96
Nikita Pestov <grizzlyarchi@gmail.com>
2015-10-13 14:05:44 +0500

    showTable added

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ var phoneBook = []; // Здесь вы храните записи как хот
 module.exports.add = function add(name, phone, email) {
     if (typeof(name) === 'string' && typeof(phone) === 'string' && typeof(email) === 'string') {
         var lowerEmail = email.toLocaleLowerCase();
-        for (var i in phoneBook) {
+        for (var i = 0; i < phoneBook.length; i++) {
             if (phoneBook[i].name.toLocaleLowerCase() === name.toLocaleLowerCase()) {
                 return undefined;
             }
@@ -53,7 +53,7 @@ module.exports.find = function find(query) {
     var result = [];
     // Ваша удивительная магия здесь
     var lowerQuery = query.toLocaleLowerCase();
-    for (var i in phoneBook) {
+    for (var i = 0; i < phoneBook.length; i++) {
         var curRecord = phoneBook[i];
         var lowerName = curRecord.name.toLocaleLowerCase();
         if (lowerName.indexOf(lowerQuery) > -1) {
@@ -69,7 +69,7 @@ module.exports.find = function find(query) {
             continue;
         }
     }
-    for (i in result) {
+    for (i = 0; i < result.length; i++) {
         console.log(result[i].name + ', ' + result[i].phone + ', ' + result[i].email);
     }
 };
@@ -82,7 +82,7 @@ module.exports.remove = function remove(query) {
     // Ваша необьяснимая магия здесь
     var counter = 0;
     var lowerQuery = query.toLocaleLowerCase();
-    for (var i in phoneBook) {
+    for (var i = 0; i < phoneBook.length; i++) {
         var curRecord = phoneBook[i];
         var lowerName = curRecord.name.toLocaleLowerCase();
         if (lowerName.indexOf(lowerQuery) > -1) {
@@ -113,19 +113,111 @@ module.exports.importFromCsv = function importFromCsv(filename) {
     // Ваша чёрная магия:
     // - Разбираете записи из `data`
     // - Добавляете каждую запись в книгу
-    data =  data.split('\n');
+    data =  data.split('\r\n');
     for (var i = 0; i < data.length; i++) {
         var record = data[i].split(';');
         module.exports.add(record[0], record[1], record[2]);
     }
-
 };
 
 /*
    Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
 */
+
+var drawTopLine = function(lengthName, lengthPhone, lengthEmail) {
+    var line = '┌'
+    for (var i = 0; i < lengthName + 4; i++) {
+        line += '─';
+    }
+    line += '┬';
+    for (var i = 0; i < lengthPhone + 4; i++) {
+        line += '─';
+    }
+    line += '┬';
+    for (var i = 0; i < lengthEmail + 4; i++) {
+        line += '─';
+    }
+    line += '┐';
+    return line;
+};
+
+var drawMiddleLine = function(lengthName, lengthPhone, lengthEmail) {
+    var line = '├'
+    for (var i = 0; i < lengthName + 4; i++) {
+        line += '─';
+    }
+    line += '┼';
+    for (var i = 0; i < lengthPhone + 4; i++) {
+        line += '─';
+    }
+    line += '┼';
+    for (var i = 0; i < lengthEmail + 4; i++) {
+        line += '─';
+    }
+    line += '┤';
+    return line;
+};
+
+var drawBottomLine = function(lengthName, lengthPhone, lengthEmail) {
+    var line = '└'
+    for (var i = 0; i < lengthName + 4; i++) {
+        line += '─';
+    }
+    line += '┴';
+    for (var i = 0; i < lengthPhone + 4; i++) {
+        line += '─';
+    }
+    line += '┴';
+    for (var i = 0; i < lengthEmail + 4; i++) {
+        line += '─';
+    }
+    line += '┘';
+    return line;
+};
+
+var drawLine = function(name, lengthName, phone, lengthPhone, email, lengthEmail) {
+    var line = '│';
+    while (name.length < lengthName) {
+        name += ' ';
+    }
+    while (phone.length < lengthPhone) {
+        phone += ' ';
+    }
+    while (email.length < lengthEmail) {
+        email += ' ';
+    }
+    line += '  ' + name + '  │  ' + phone + '  │  ' + email + '  │';
+    return line;
+};
+
 module.exports.showTable = function showTable(filename) {
+    if (phoneBook.length === 0) {
+        console.log('Телефонная книга пуста');
+        return undefined;
+    }
 
+    var maxNameLength = 0;
+    var maxPhoneLength = 0;
+    var maxEmailLength = 0;
     // Ваша чёрная магия здесь
-
+    for (var i = 0; i < phoneBook.length; i++) {
+        var currentRecord = phoneBook[i];
+        if (currentRecord.name.length > maxNameLength) {
+            maxNameLength = currentRecord.name.length;
+        }
+        if (currentRecord.phone.length > maxPhoneLength) {
+            maxPhoneLength = currentRecord.phone.length;
+        }
+        if (currentRecord.email.length > maxEmailLength) {
+            maxEmailLength = currentRecord.email.length;
+        }
+    }
+    console.log(drawTopLine(maxNameLength, maxPhoneLength, maxEmailLength));
+    console.log(drawLine('Имя', maxNameLength, 'Телефон', maxPhoneLength, 'email', maxEmailLength));
+    console.log(drawMiddleLine(maxNameLength, maxPhoneLength, maxEmailLength));
+    for (var i = 0; i < phoneBook.length; i++) {
+        var cur = phoneBook[i];
+        console.log(drawLine(cur.name, maxNameLength, cur.phone, maxPhoneLength, cur.email, maxEmailLength))
+    }
+    console.log(drawBottomLine(maxNameLength, maxPhoneLength, maxEmailLength));
 };
